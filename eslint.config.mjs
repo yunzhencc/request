7d616ea83cddb9d8d233cdf09ca6eca80dfcc59c
wangxingkang <wang_xingkang@qq.com>
2025-08-21 14:16:48 +0800

    chore: add semantic-release

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,10 @@
 import antfu from '@antfu/eslint-config';
-import pluginQuery from '@tanstack/eslint-plugin-query';
 
 export default antfu(
   {
     stylistic: {
       semi: true,
     },
-    
     ignores: [
       'dist',
     ],
